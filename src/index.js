e57956fd2c2db9137b156b9b6764dfeae531824f
Elvira-g <elvira_27@mail.ru>
2022-01-29 12:52:01 +0300

    test completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let nString = String(n);
+    let nArr = [];
+    
+    for( i = 0; i < nString.length; i++){
+        if(nString[0] === '-'){
+            nString = nString.slice(1);
+        }
+        nArr.push(nString[i]);
+    }
+    return Number(nArr.reverse().join(''));
 }
